18349afd80455fa5aaa68509dc1a974c4f1bad3b
b0n3 <lalouabd123@gmail.com>
2020-06-18 17:20:18 -0400

    fp

---
+++ flutter_application/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/b0n3/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/b0n3/Desktop/flu/codecanyon-26409320-grocery-food-pharmacy-store-delivery-mobile-app-with-admin-panel/flutter_application"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/home/b0n3/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.3.0"
+export "FLUTTER_BUILD_NUMBER=1.3.0"
---
+++ flutter_application/ios/Runner/AppDelegate.swift
@@ -0,0 +1,13 @@
+import UIKit
+import Flutter
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [AIzaSyCx116rwLx9_GOBbVUpaMHw2j9TmGQEnbQ: Any]?
+  ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
